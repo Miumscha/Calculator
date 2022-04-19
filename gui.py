b4a0be7f4e3e25b6c65a46b54272ee4a98a37568
Michael Umschaden <umschaden.michael@gmx.at>
2022-04-19 17:22:31 +0200

    Added clear function, calculate function and clear single char function

--- gui.py
+++ gui.py
@@ -7,7 +7,7 @@ class CalculatorGUI:
 
     def __init__(self):
         self.entry = tk.Entry(main, bg="#FFFFFF", bd=5, relief="sunken")
-        self.entry.grid(row=0, column=0, columnspan=4, sticky="we")
+        self.entry.grid(row=0, column=0, columnspan=6, sticky="we")
         self.init_buttons()
         main.mainloop()
 
@@ -16,6 +16,8 @@ class CalculatorGUI:
         tk.Button(main, text="root", width=7).grid(row=1, column=1)
         tk.Button(main, text="%", width=7).grid(row=1, column=2)
         tk.Button(main, text="1/x", width=7).grid(row=1, column=3)
+        tk.Button(main, text="(", width=7, command=lambda: self.writetoentry("(")).grid(row=1, column=4)
+        tk.Button(main, text=")", width=7, command=lambda: self.writetoentry(")")).grid(row=1, column=5)
         tk.Button(main, text="+", width=7, command=lambda: self.writetoentry("+")).grid(row=2, column=3)
         tk.Button(main, text="1", width=7, command=lambda: self.writetoentry("1")).grid(row=2, column=0)
         tk.Button(main, text="2", width=7, command=lambda: self.writetoentry("2")).grid(row=2, column=1)
@@ -32,12 +34,23 @@ class CalculatorGUI:
         tk.Button(main, text=".", width=7, command=lambda: self.writetoentry(".")).grid(row=5, column=0)
         tk.Button(main, text="clear", width=7, command=self.clear).grid(row=5, column=1)
         tk.Button(main, text="0", width=7, command=lambda: self.writetoentry("0")).grid(row=5, column=2)
+        tk.Button(main, text="ce", width=7, command=self.clearelement).grid(row=5, column=4)
+        tk.Button(main, text="=", width=7, command=self.calculatetext).grid(row=5, column=5)
 
     def clear(self):
         self.entry.delete(0, tk.END)
 
+    def clearelement(self):
+        self.entry.delete(self.entry.index(tk.END) - 1)
+
     def writetoentry(self, text):
         self.entry.insert(tk.END, text)
+        return
+
+    def calculatetext(self):
+        result = self.entry.get()
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, eval(result))
         return
 
 
